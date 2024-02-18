38ee2bfd31e0afb9fb466f94bad02bf078e3de75
Frank Villaro-Dixon <frank@villaro-dixon.eu>
2024-02-19 00:49:49 +0100

    make it betterer
    Signed-off-by: Frank Villaro-Dixon <frank@villaro-dixon.eu>

--- src/consumer/mod.rs
+++ src/consumer/mod.rs
@@ -1,21 +1,26 @@
-use std::{println};
+use core::fmt::Error;
+use std::println;
 
-use alloc::{
-    boxed::Box,
-    vec::{Vec},
-};
+use alloc::{boxed::Box, vec::Vec};
 extern crate alloc;
 extern crate std;
 
-//use super::RB_MAGIC;
+use super::RB_MAGIC;
+
+#[derive(Debug)]
+pub struct ConsumerError(ConsumerErrorKind);
+
+#[derive(Debug)]
+pub enum ConsumerErrorKind {
+    MagicMarkerNotFound,
+    RingBufferSizeNull,
+    ReadMemoryError(Error),
+    WriteMemoryError(Error),
+}
 
 pub trait MemoryReader {
-    fn read_memory(
-        &mut self,
-        address: usize,
-        buffer: &mut [u8],
-    ) -> Result<(), alloc::string::String>;
-    fn write_memory(&mut self, address: usize, value: u8) -> Result<(), alloc::string::String>;
+    fn read_memory(&mut self, address: usize, buffer: &mut [u8]) -> Result<(), Error>;
+    fn write_memory(&mut self, address: usize, value: u8) -> Result<(), Error>;
 }
 
 pub struct ProducerDevice<'a> {
@@ -33,27 +38,27 @@ impl<'a> ProducerDevice<'a> {
     pub fn new(
         mut memory_reader: Box<dyn MemoryReader>,
         ram_start_address: usize,
-    ) -> Result<ProducerDevice<'a>, ()> {
+    ) -> Result<ProducerDevice<'a>, ConsumerError> {
         //First of all, ensure that we have the correct base address of the RB
         let mut magic_markers = [0; 3];
-        memory_reader.read_memory(ram_start_address, &mut magic_markers);
+        match memory_reader.read_memory(ram_start_address, &mut magic_markers) {
+            Err(x) => return Err(ConsumerError(ConsumerErrorKind::ReadMemoryError(x))),
+            _ => (),
+        }
 
-        const WANT_MAGIC: [u8; 3] = [0x88, 0x88, 0x88]; // XXX to share amongst prod/cons
-        if magic_markers != WANT_MAGIC {
-            //return Err("Magic markers don't start with 0x88".to_string());
-            panic!(
-                "Magic markers arent {:02x?}: {:02x?}",
-                WANT_MAGIC, magic_markers
-            );
-            return Err(());
+        if magic_markers != RB_MAGIC {
+            return Err(ConsumerError(ConsumerErrorKind::MagicMarkerNotFound));
         }
 
         //Get the size of the RB
         let mut buf = [0; 1];
-        memory_reader.read_memory(ram_start_address + 3, &mut buf);
+        match memory_reader.read_memory(ram_start_address + ADDR_SIZE, &mut buf) {
+            Err(x) => return Err(ConsumerError(ConsumerErrorKind::ReadMemoryError(x))),
+            _ => (),
+        }
         let rb_size = buf[0];
         if rb_size == 0 {
-            return Err(());
+            return Err(ConsumerError(ConsumerErrorKind::RingBufferSizeNull));
         }
 
         println!("The RB is of size {}", rb_size);
@@ -65,21 +70,20 @@ impl<'a> ProducerDevice<'a> {
         })
     }
 
-    fn read_one_byte(&mut self, address: usize) -> Result<u8, ()> {
+    fn read_one_byte(&mut self, address: usize) -> Result<u8, ConsumerError> {
         // Basically the same as read_memory, but only reads
         // one byte. Easier than allocating a buffer, etc..
 
         let mut buf = [0u8; 1];
         match self.memory_reader.read_memory(address, &mut buf) {
             Ok(_) => Ok(buf[0]),
-            Err(_x) => Err(()),
+            Err(x) => Err(ConsumerError(ConsumerErrorKind::ReadMemoryError(x))),
         }
     }
 
-    pub fn read_bytes(&mut self) -> Vec<u8> {
+    pub fn read_bytes(&mut self) -> Result<Vec<u8>, ConsumerError> {
         let mut bytes: Vec<u8> = alloc::vec![];
 
-        //for mem_addr in 0x3f00..0x3f0f {
         let prod_a = self.ram_start + ADDR_PROD;
         let cons_a = self.ram_start + ADDR_CONS;
         let buff_a = self.ram_start + ADDR_BUFF;
@@ -90,11 +94,13 @@ impl<'a> ProducerDevice<'a> {
         while prod_v != cons_v {
             let buff_a = buff_a + cons_v as usize;
             let buff_v = self.read_one_byte(buff_a).unwrap(); // XXX TODO: we could read the whole range
-            cons_v = (cons_v + 1) % 5;
+            cons_v = (cons_v + 1) % self.rb_size;
             bytes.push(buff_v);
-            //            println!("RCVD: {:02x?}", buff_v);
-            self.memory_reader.write_memory(cons_a, cons_v);
+            match self.memory_reader.write_memory(cons_a, cons_v) {
+                Err(x) => return Err(ConsumerError(ConsumerErrorKind::WriteMemoryError(x))),
+                _ => (),
+            }
         }
-        bytes
+        Ok(bytes)
     }
 }
